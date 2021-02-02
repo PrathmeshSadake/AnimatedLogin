9040ec4b13b9ebc7952bf9e7dfc5065a36f1831f
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-02-02 16:09:46 +0530

    Finished LoginPage

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,69 +7,104 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  int _pageState = 0;
+
+  var _backgroundColor = Colors.white;
+  var _fontColor = Colors.black;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 100, bottom: 30),
+    switch (_pageState) {
+      case 0:
+        _backgroundColor = Colors.white;
+        // _fontColor = Colors.black;
+        break;
+      case 1:
+        _backgroundColor = Color(0xFFB2EBF2);
+        // _fontColor = Colors.white;
+        break;
+      case 2:
+        _backgroundColor = Colors.white;
+        // _fontColor = Colors.black;
+        break;
+    }
+    //https://assets1.lottiefiles.com/packages/lf20_rycdh53q.json
+    return AnimatedContainer(
+      duration: Duration(
+        milliseconds: 500,
+      ),
+      color: _backgroundColor,
+      padding: EdgeInsets.only(top: 100, bottom: 30),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
             padding: EdgeInsets.symmetric(
-              horizontal: 30,
+              horizontal: 20,
             ),
             child: Column(
               children: [
                 Text(
                   'Learn Free',
                   style: TextStyle(
-                    fontSize: 23.0,
+                    fontSize: 25.0,
+                    letterSpacing: 1.25,
+                    color: _fontColor,
                   ),
                 ),
                 SizedBox(
                   height: 15,
                 ),
                 Text(
-                  'We make learning easy. Join our coding bootcamp for free and also don\'t forget to follow us on social media.',
+                  'Join our coding bootcamp and get opportunity to work as a software developer in top IT companies.',
                   style: TextStyle(
-                    fontSize: 17,
+                    fontSize: 18,
+                    color: _fontColor,
+                    letterSpacing: 1,
                   ),
                   textAlign: TextAlign.center,
                 ),
               ],
             ),
           ),
+          SizedBox(
+            height: 30,
+          ),
           Container(
             padding: EdgeInsets.symmetric(
-              horizontal: 20,
+              horizontal: 10,
             ),
-            child: Image.asset(
-              'assets/images/splash_bg.png',
-              fit: BoxFit.cover,
-            ),
+            child: Lottie.asset('assets/images/learning.json'),
           ),
+          Spacer(),
           Container(
             margin: EdgeInsets.symmetric(
               horizontal: 28,
             ),
-            child: Container(
-              width: double.infinity,
-              padding: EdgeInsets.all(14),
-              decoration: BoxDecoration(
-                color: Color(
-                  0xFFB40284A,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  _pageState = (_pageState == 1) ? 0 : 1;
+                });
+              },
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(14),
+                decoration: BoxDecoration(
+                  color: Color(
+                    0xFF00B0FF,
+                  ),
+                  borderRadius: BorderRadius.circular(
+                    50,
+                  ),
                 ),
-                borderRadius: BorderRadius.circular(
-                  50,
-                ),
-              ),
-              child: Text(
-                'Get Started',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 1.75,
-                  fontSize: 20.0,
+                child: Text(
+                  'Get Started',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 1.75,
+                    fontSize: 20.0,
+                  ),
                 ),
               ),
             ),
